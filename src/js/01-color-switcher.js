7d85c438901e96dd92a581edaeec33c0fd78094d
Natalia Kozak <90988795+Natalja21@users.noreply.github.com>
2022-07-21 14:58:09 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+console.log(startBtnRef);
+console.log(stopBtnRef);
+let timerId = null;
 
+// генерування випадкового кольору
+
+const getRandomHexColor = () =>
+  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const onChangeBg = () => {
+  timerId = setInterval(
+    () => (bodyRef.style.background = getRandomHexColor()),
+    1000
+  );
+  startBtnRef.disabled = true;
+  stopBtnRef.disabled = false;
+};
+
+const stopChangeBg = () => {
+  clearInterval(timerId);
+  startBtnRef.disabled = false;
+  stopBtnRef.disabled = true;
+};
+
+startBtnRef.addEventListener('click', onChangeBg);
+stopBtnRef.addEventListener('click', stopChangeBg);
