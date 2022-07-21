e69150013247b3548e425fe42b7ea6c58668126b
Natalja21 <90988795+Natalja21@users.noreply.github.com>
2022-07-21 11:58:44 +0000

    Deploying to gh-pages from @ Natalja21/goit-js-hw-09@7d85c438901e96dd92a581edaeec33c0fd78094d 🚀

---
+++ 01-color-switcher.abf9954f.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),n=document.querySelector("body");console.log(e),console.log(t);var o=null;e.addEventListener("click",(function(){o=setInterval((function(){return n.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(o),e.disabled=!1,t.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.abf9954f.js.map
