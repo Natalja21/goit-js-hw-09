cff34a98b400f4d88fd901045227c88ef0f01e83
Natalia Kozak <90988795+Natalja21@users.noreply.github.com>
2022-07-21 15:06:49 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,29 +1,30 @@
 const startBtnRef = document.querySelector('[data-start]');
 const stopBtnRef = document.querySelector('[data-stop]');
 const bodyRef = document.querySelector('body');
-console.log(startBtnRef);
-console.log(stopBtnRef);
+
 let timerId = null;
 
-// генерування випадкового кольору
-
+                                    // генерування випадкового кольору
 const getRandomHexColor = () =>
-  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
+                                    // кнопка старт для зміни background
 const onChangeBg = () => {
-  timerId = setInterval(
-    () => (bodyRef.style.background = getRandomHexColor()),
-    1000
-  );
-  startBtnRef.disabled = true;
-  stopBtnRef.disabled = false;
+    timerId = setInterval(
+        () => (bodyRef.style.background = getRandomHexColor()),
+        1000
+    );
+    startBtnRef.disabled = true;
+    stopBtnRef.disabled = false;
 };
 
+                                     // кнопка stop
 const stopChangeBg = () => {
-  clearInterval(timerId);
-  startBtnRef.disabled = false;
-  stopBtnRef.disabled = true;
+    clearInterval(timerId);
+    startBtnRef.disabled = false;
+    stopBtnRef.disabled = true;
 };
 
+                                     //  addEventListener
 startBtnRef.addEventListener('click', onChangeBg);
 stopBtnRef.addEventListener('click', stopChangeBg);
